bd875040a737b0fb3952a88ecdcf199cd8ca145a
chapter911 <51829448+chapter911@users.noreply.github.com>
2025-03-16 00:53:37 +0700

    add custom ringtone

--- android/app/src/main/kotlin/com/zizi/secure/MainActivity.kt
+++ android/app/src/main/kotlin/com/zizi/secure/MainActivity.kt
@@ -1,5 +1,41 @@
 package com.zizi.secure
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.media.AudioAttributes
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initChatChannel()
+    }
+
+    private fun initChatChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = getString(R.string.message_channel_id)
+            val name = getString(R.string.message_channel_name)
+            val descriptionText = getString(R.string.message_channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val mChannel = NotificationChannel(channelId, name, importance)
+            mChannel.description = descriptionText
+            // Set the custom sound URI
+            val soundName = "raw/message_tone"
+            val soundUri =
+                Uri.parse("android.resource://${context.packageName}/" + soundName)
+
+            val audioAttributes = AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                .build()
+
+            mChannel.setSound(soundUri, audioAttributes)
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+        }
+    }
+}
